f22f104e348539363b862e4e7b04ec187e7e3ee3
Andrey Falinsky <AndreyFalinsky@coherentsolutions.com>
2018-08-27 18:20:52 +0300

    Emiter v1

---
+++ DemoRPC/RPCServer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace DemoRPC
+{
+    internal class RpcServer
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672, Password = "guest", UserName = "guest" };
+            using (var connection = factory.CreateConnection())
+            using (var chanel = connection.CreateModel())
+            {
+                chanel.QueueDeclare(queue: "rpc_queue", durable: false, exclusive: false, autoDelete: false,
+                    arguments: null);
+                chanel.BasicQos(0,1,false);
+                var consumer = new EventingBasicConsumer(chanel);
+                chanel.BasicConsume(queue: "rpc_queue", autoAck: false, consumer: consumer);
+                Console.WriteLine(" [x] Awaiting RPC requests");
+
+                consumer.Received += (model, ea) =>
+                {
+                    string response = null;
+                    var body = ea.Body;
+                    var props = ea.BasicProperties;
+                    var replyProps = chanel.CreateBasicProperties();
+                    replyProps.CorrelationId = props.CorrelationId;
+
+                    try
+                    {
+                        var message = Encoding.UTF8.GetString(body);
+                        int n = int.Parse(message);
+                        Console.WriteLine($" [.] fib({message})");
+                        response = Fib(n).ToString();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(" [.] " + e.Message);
+                        response = "";
+                    }
+                    finally
+                    {
+                        var responseBytes = Encoding.UTF8.GetBytes(response);
+                        chanel.BasicPublish(exchange:"", routingKey:props.ReplyTo, basicProperties:replyProps, body:responseBytes);
+                        chanel.BasicAck(deliveryTag:ea.DeliveryTag, multiple:false);
+                    }
+                };
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+
+        }
+
+        private static int Fib(int n)
+        {
+            if (n == 0 || n == 1) return n;
+
+            return Fib(n - 1) + Fib(n - 2);
+        }
+    }
+}
---
+++ DemoRebbitMQ_Console/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace DemoRebbitMQ_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() {HostName = "localhost", Port = 5672, Password = "guest", UserName = "guest"};
+            using (var connection = factory.CreateConnection())
+            {
+                using (var chanel = connection.CreateModel())
+                {
+                    
+                    chanel.ExchangeDeclare(exchange: "topic_logs", type:"topic");
+
+                    var routingKey = (args.Length > 0) ? args[0] : "anonymous.info";
+
+                    var message = GetMessage(args);
+
+                    var body = Encoding.UTF8.GetBytes(message);
+                  
+                    chanel.BasicPublish(exchange: "topic_logs", routingKey: routingKey, basicProperties:null, body:body);
+
+                    Console.WriteLine($" [x] Sent {routingKey}:{message}");
+                    Console.ReadLine();
+                }
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return args.Length > 1
+                ? string.Join(" ", args.Skip(1).ToArray())
+                : "Hello World!";
+        }
+    }
+}
